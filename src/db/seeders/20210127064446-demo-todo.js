d3448b036a2002db8d766982a91fb7bd48196859
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2021-01-27 13:57:34 +0700

    #5 seeder

---
+++ src/db/seeders/20210127064446-demo-todo.js
@@ -0,0 +1,33 @@
+'use strict';
+const faker = require('faker')
+
+const todos = (() => {
+  const data = []
+  for (let i = 0; i < 50; i++) {
+    data.push(
+      {
+        title: faker.lorem.sentence(),
+        desc: faker.lorem.sentence(),
+        status: true,
+        createdAt: faker.date.recent(),
+        updatedAt: faker.date.recent()
+      }
+    )
+  }
+  return data
+})()
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Todos', todos)
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
