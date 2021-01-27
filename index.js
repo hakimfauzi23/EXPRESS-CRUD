31afe32c0a5ef6ada577539522f3290b98bb8896
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2021-01-27 14:27:16 +0700

    #7 Create Data

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const express = require('express')
-const TodoController = require('./src/controller/todoController')
+const TodoController = require('./src/controller/TodoController')
 const app = express()
 const port = 3000
 
-app.get('todos/', TodoController.get)
+app.get('/todos', TodoController.get)
+app.get('/todos/:id', TodoController.getById)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
-const { Sequelize } = require('sequelize');
-const db = require('./src/config/database')
+// const { Sequelize } = require('sequelize');
+// const db = require('./src/config/database')
 
-const sequelize = new Sequelize(db.development)
+// const sequelize = new Sequelize(db.development)
 
-try {
-    sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-} catch (error) {
-    console.error('Unable to connect to the database:', error);
-}
+// try {
+//     sequelize.authenticate();
+//     console.log('Connection has been established successfully.');
+// } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+// }
---
+++ src/controller/TodoController.js
@@ -0,0 +1,15 @@
+const {Todo} = require('../db/models')
+
+class TodoController {
+
+    static async get(req, res) {
+        const data = await Todo.findAll() 
+        res.json(data) 
+    }
+
+    static async getById(req, res) {
+        const data = await Todo.findByPk(req.params["id"]) 
+        res.json(data) 
+    }
+}
+module.exports = TodoController;
