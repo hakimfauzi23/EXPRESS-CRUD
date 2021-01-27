9e1a1d3e7694815bede2257e40a6a911cf5b5314
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2021-01-27 13:32:11 +0700

    #3 config sequelize

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ const app = express()
 const port = 3000
 
 app.get('todos/', TodoController.get)
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+const { Sequelize } = require('sequelize');
+const db = require('./src/config/database')
+
+const sequelize = new Sequelize(db.development)
+
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
